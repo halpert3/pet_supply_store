966fd33459657ad496e25c26c7b87aa682bdbb16
Henry Alpert <halpert3@gmail.com>
2017-11-27 16:26:19 +0000

    Add filter by category

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,6 +1,12 @@
 class StorefrontController < ApplicationController
   def index
-    @products = Product.all
+  
+    if params[:cat_id]
+      @category = Category.find(params[:cat_id])
+      @products = Product.where(category_id: @category.id)
+    else
+      @products = Product.all
+    end
   end
 
   def about
