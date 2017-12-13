cae268cfafc18214e5f84269001334807aeae02a
Henry Alpert <halpert3@gmail.com>
2017-12-13 16:42:15 +0000

    Set up mailers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'tts-halpert3.c9users.io' }
-  config.action_mailer.delivery_method = :letter_opener
+  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp 
+  # config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'tts-halpert3.c9users.io',
+    authetication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['gmail_username'],
+    password: ENV['gmail_password']
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,26 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  
+  config.action_mailer.default_url_options = { host: 'tts-halpert3.c9users.io' }
+  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp 
+  # config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV['domain'], #from application yaml file
+    authetication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['gmail_username'],
+    password: ENV['gmail_password']
+  }
+
+  config.action_mailer.default_url_options = { host: ENV['domain'] }
+  confi.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
